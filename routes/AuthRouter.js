28f6253566a18e6c9c2bb8d15d1d1a55c42667e0
denizozmen <mr.denizozmen@gmail.com>
2020-12-12 22:15:38 +0300

    Add auth middleware & jwt & express validator

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const router = express.Router();
 
@@ -10,13 +11,35 @@ const AuthController = require("../controllers/AuthController");
  * @desc    Register endpoint
  * @access  Public
  */
-router.post("/register", AuthController.auth_register);
+router.post(
+  "/register",
+  [
+    check("password", "Please enter a password with 6 and more chars").isLength(
+      {
+        min: 6,
+      }
+    ),
+    check("email", "Please enter a valid email!").isEmail(),
+  ],
+  AuthController.authRegister
+);
 
 /**
  * @route   POST /api/auth/login
  * @desc    Login endpoint
- * @access  Private
+ * @access  Public
  */
-router.post("/login", AuthController.auth_login);
+router.post(
+  "/login",
+  [
+    check("email", "Please enter a valid email!").isEmail(),
+    check("password", "Please enter a password with 6 and more chars").isLength(
+      {
+        min: 6,
+      }
+    ),
+  ],
+  AuthController.authLogin
+);
 
 module.exports = router;
