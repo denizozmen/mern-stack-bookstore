4108ececf206b7566559fc92b4e7966b29b9b49d
denizozmen <mr.denizozmen@gmail.com>
2020-12-12 17:44:52 +0300

    Add save user model

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,15 +1,27 @@
-exports.auth_register = (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
-  console.log("Fields:", firstName, lastName, email, password);
-  //TODO: Validate the fields
-  //TODO2:Already check registered
-  //TODO3:crpyt password
-  //TODO4:save the user to DB
+const User = require("../models/UserModel");
 
+exports.authRegister = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  // TODO1: Validate the fields
+  // TODO2: check already registered
+  // TODO3: crpyt password
+  // TODO4: save the user to DB
+
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    password, //crypted password
+  });
+
+  await user.save();
+
+  //TODO: Error handling for saving
   res.send("Register Completed.");
 };
 
-exports.auth_login = (req, res) => {
+exports.authLogin = (req, res) => {
   // TODO: Auth.
   // TODO: Login func.
   res.send("Login Completed");
