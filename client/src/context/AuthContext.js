a4ccd49d76657fd36d96870075470e3f833e7bcb
denizozmen <mr.denizozmen@gmail.com>
2020-12-17 13:47:09 +0300

    Add prodcution case

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,9 @@
+import { createContext, useState, useEffect } from "react";
+
+export const FirebaseAuthContext = createContext();
+
+function AuthContextProvider(props) {
+  return <div />;
+}
+
+export default AuthContextProvider;
